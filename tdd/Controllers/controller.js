4b3b0be746be3efef0eb5b2d4f4cf9f3dab6a778
Diel Duarte <diel@Diels-MacBook-Pro.local>
2015-08-02 17:36:56 -0300

    tdd in controllers

---
+++ tdd/Controllers/controller.js
@@ -0,0 +1,22 @@
+(function(){
+	'use strict';
+
+	angular.module('notesApp', [])
+		.controller('listCtrl', listCtrl);
+
+		function listCtrl() {
+			var self = this;
+
+			self.items = [
+				{ id: 1, lavel: 'first', done: true },
+				{ id: 2, lavel: 'second', done: false }
+			];
+
+			self.getDoneClass = function(item){
+				return {
+					finished: item.done,
+					unfinished: !item.done
+				};
+			};
+		}
+})();
---
+++ tdd/Controllers/controllerSpec.js
@@ -0,0 +1,34 @@
+describe('Controller: listCtrl', function(){
+	//instanciando o modulo antes de cada it
+	beforeEach(module('notesApp'));
+
+	var ctrl;
+
+	//instancio o controller antes de cada it
+	beforeEach(inject(function($controller){
+		ctrl = $controller('listCtrl');
+	}));
+
+	it('should have items available on load', function(){
+		expect(ctrl.items).toEqual([
+			{ id: 1, lavel: 'first', done: true },
+			{ id: 2, lavel: 'second', done: false }
+		]);
+	});
+
+	it('should have highlight items based on state', function(){
+		var item = { id: 1, lavel: 'first', done: true };
+		var actualClass = ctrl.getDoneClass(item);
+
+		expect(actualClass.finished).toBeTruthy();
+		expect(actualClass.unfinished).toBeFalsy();
+
+		item.done = false;
+
+		actualClass = ctrl.getDoneClass(item);
+		expect(actualClass.finished).toBeFalsy();
+		expect(actualClass.unfinished).toBeTruthy();
+
+	});
+
+});
