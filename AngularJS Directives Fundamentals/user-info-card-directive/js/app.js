e70c0c8f690a2e8e3b1691551c1d3e5a907e5acd
Diel Duarte <diel@Diels-MacBook-Pro.local>
2015-10-05 13:56:22 -0300

    user card directive

--- AngularJS Directives Fundamentals/user-info-card-directive/js/app.js
+++ AngularJS Directives Fundamentals/user-info-card-directive/js/app.js
@@ -2,11 +2,32 @@
 
 	angular
 		.module('app', [])
-		.controller('MainController', MainController);
+		.controller('MainController', MainController)
+		.directive('userInfoCard', userInfoCard);
 
-		function MainController(){
+		function MainController($scope){
 			var vm = this;
 			vm.oi = 'oii';
+
+			$scope.user = {
+				name: 'diel duarte',
+				address: {
+					city: 'betim',
+					phone: '31 92263265'
+				},
+				friends: [
+					'pedro',
+					'marcio',
+					'claudio'
+				]
+			}
 		}
 
+
+		function userInfoCard(){
+			return {
+				templateUrl: "userInfoCard.html",
+				restrict: 'E'
+			}
+		}
 })();
