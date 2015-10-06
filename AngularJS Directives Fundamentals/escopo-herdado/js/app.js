8f845d41a7bf26572bd785641587ec7ff90513a7
Diel Duarte <diel@Diels-MacBook-Pro.local>
2015-10-06 10:54:12 -0300

    escopo herdado

--- AngularJS Directives Fundamentals/escopo-herdado/js/app.js
+++ AngularJS Directives Fundamentals/escopo-herdado/js/app.js
@@ -3,11 +3,12 @@
 	angular
 		.module('app', [])
 		.controller('MainController', MainController)
-		.directive('userInfoCard', userInfoCard);
+		.directive('userInfoCard', userInfoCard)
+		.directive('address', address);
 
 		function MainController($scope){
 			var vm = this;
-			vm.oi = 'oi';
+			vm.oi = 'oii';
 
 			vm.user = {
 				name: 'diel duarte',
@@ -48,6 +49,26 @@
 				controller: function($scope){			
 					$scope.knightMe = function(user) {
 						user.rank = 'knight';
+					}
+					$scope.collapse = function() {
+						$scope.collapsed = !$scope.collapsed;
+					}
+				}
+			}
+		} //end userInfoCard
+
+		function address(){
+			return {
+				templateUrl: 'address.html',
+				restrict: 'E',
+				scope: true,
+				controller: function($scope){
+					$scope.collapsed = false;
+					$scope.collapseAddress = function() { 
+							$scope.collapsed = true;
+					}
+					$scope.expandAddress = function() { 
+							$scope.collapsed = false;
 					}
 				}
 			}
